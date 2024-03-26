3335ec68a897e919932efc5585491c18dc54aec0
RohitMalviya <rohitbmalviya@gmail.com>
2024-03-26 12:28:15 +0530

    trying the responsiveness

--- next-typescript/src/components/Navbar.tsx
+++ next-typescript/src/components/Navbar.tsx
@@ -3,10 +3,18 @@ import Link from "next/link";
 export default function Navbar() {
   return (
     <>
-      <nav className="bg-slate-300">
-        <Link href={"/"}>Home</Link>
-        <Link href={"/allcourse"}>All Course</Link>
-        <Link href={"/contactus"}>Contact Us</Link>
+      <nav className="bg-black  w-full flex justify-center items-center space-y-4 h-20 ">
+        <ul className="min-[400px]:flex hidden lg:flex md:flex sm:flex justify-around items-center text-3xl font-semibold text- h-16 bg-white lg:w-1/2 rounded-full px-8">
+          <li>
+            <Link href={"/"}>Home</Link>
+          </li>
+          <li>
+            <Link href={"/allcourse"}>All Course</Link>
+          </li>
+          <li>
+            <Link href={"/contactus"}>Contact Us</Link>
+          </li>
+        </ul>
       </nav>
     </>
   );
