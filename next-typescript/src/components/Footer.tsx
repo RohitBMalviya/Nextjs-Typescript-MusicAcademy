6dc4e79be29c4f58475751753628708734d4bc88
RohitMalviya <rohitbmalviya@gmail.com>
2024-04-07 00:48:15 +0530

    ....

--- next-typescript/src/components/Footer.tsx
+++ next-typescript/src/components/Footer.tsx
@@ -9,26 +9,26 @@ import Image from "next/image";
 export default function Footer(): JSX.Element {
   return (
     <>
-      <footer className="bg-gray-900 h-full py-14 px-10 text-white grid min-[1440px]:grid-cols-4 lg:grid-cols-2 sm:grid-cols-2 gap-6 items-center">
-        <div className="space-y-4 flex flex-col items-center h-[12rem]">
-          <h3 className="min-[2000px]:text-4xl lg:text-3xl text-2xl font-bold w-[60%] text-[#00FFFF]">
+      <footer className="bg-gray-900 h-full py-10 px-10 text-white grid min-[1440px]:grid-cols-4 lg:grid-cols-2 sm:grid-cols-2 gap-4 items-center">
+        <div className="space-y-4 flex flex-col items-center h-[12rem] max-[641px]:h-full ">
+          <h3 className="min-[2000px]:text-4xl lg:text-3xl text-2xl font-bold w-[70%] max-[641px]:w-full text-[#00FFFF]">
             About Us
           </h3>
-          <p className="min-[2000px]:text-2xl lg:text-xl text-lg w-[60%]">
+          <p className="min-[2000px]:text-2xl lg:text-xl text-lg w-[70%] max-[641px]:w-full">
             Welcome to Music Academy where passion meets proficiency in the
             realm of music education
           </p>
         </div>
-        <div className="space-y-4 flex flex-col items-center h-[12rem]">
-          <h3 className="min-[2000px]:text-4xl lg:text-3xl text-2xl font-bold w-[60%] text-[#00FFFF]">
+        <div className="space-y-4 flex flex-col items-center h-[12rem] max-[641px]:h-full">
+          <h3 className="min-[2000px]:text-4xl lg:text-3xl text-2xl font-bold w-[70%] max-[641px]:w-full text-[#00FFFF]">
             Quick Link
           </h3>
-          <menu className="min-[2000px]:text-2xl lg:text-xl text-lg w-[60%]">
+          <menu className="min-[2000px]:text-2xl lg:text-xl text-lg w-[70%] max-[641px]:w-full">
             <li>
               <Link href={"/"}>Home</Link>
             </li>
             <li>
-              <Link href={"/aboutUs"}>AboutUs</Link>
+              <Link href={"/aboutus"}>AboutUs</Link>
             </li>
             <li>
               <Link href={"/courses"}>Courses</Link>
@@ -38,11 +38,11 @@ export default function Footer(): JSX.Element {
             </li>
           </menu>
         </div>
-        <div className="space-y-4 flex flex-col items-center h-[12rem]">
-          <h3 className="min-[2000px]:text-4xl lg:text-3xl text-2xl font-bold w-[60%] pl-2 text-[#00FFFF]">
+        <div className="space-y-4 flex flex-col items-center h-[12rem] max-[641px]:h-full">
+          <h3 className="min-[2000px]:text-4xl lg:text-3xl text-2xl font-bold w-[70%] max-[641px]:w-full pl-2 text-[#00FFFF]">
             Follow Us
           </h3>
-          <ul className="min-[2000px]:text-2xl lg:text-xl text-lg w-[60%] flex ">
+          <ul className="min-[2000px]:text-2xl lg:text-xl text-lg w-[70%] max-[641px]:w-full flex ">
             <li>
               <Link href="#">
                 <Image src={Iicon} alt="Instagram" />
@@ -65,11 +65,11 @@ export default function Footer(): JSX.Element {
             </li>
           </ul>
         </div>
-        <div className="space-y-4 flex flex-col items-center h-[12rem]">
-          <h3 className="min-[2000px]:text-4xl lg:text-3xl text-2xl font-bold w-[60%] text-[#00FFFF]">
+        <div className="space-y-4 flex flex-col items-center h-[12rem] max-[641px]:h-full">
+          <h3 className="min-[2000px]:text-4xl lg:text-3xl text-2xl font-bold w-[70%] max-[641px]:w-full text-[#00FFFF]">
             Contact Us
           </h3>
-          <address className="min-[2000px]:text-2xl lg:text-xl text-lg w-[60%]">
+          <address className="min-[2000px]:text-2xl lg:text-xl text-lg w-[70%] max-[641px]:w-full">
             Maharashtra,Bharat Pune 412115 <br />
             Email: music@academy.com <br />
             phone:+91 9876543210
